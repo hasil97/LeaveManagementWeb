df713f9a88fd0cec024df7eff581fa63b88b5bb7
Hasil97 <hasilarahman@gmail.com>
2022-12-30 11:47:09 +0530

    I have extended the Employee class onto IdentityUser

---
+++ LeaveManagementWeb/Data/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace LeaveManagementWeb.Data
+{
+    public abstract class BaseEntity //here abstract means we cannot use this class alone. This class only works when some other class inherits from this class.
+    {
+
+    }
+}
--- LeaveManagementWeb/Program.cs
+++ LeaveManagementWeb/Program.cs
@@ -1,6 +1,17 @@
+using Humanizer;
 using LeaveManagementWeb.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
+/*  1. We extend the AspNetUsers table which is a table created when we use authentication type as induvidual authentication
+    2. The name of the extended table is Employee table. So we need to create an Employee class and then inherit IdentityUser class, which is part of Microsoft.AspNetCore.Identity
+    3. Now since we want AspNetUsers to have additional coloumns present in Employee class, we need to extend the Employee class in ApplicationDbContext : IdentityDbContext<Employee>
+    4. Now we need to make changes in the program.cs. We need to change AddDefaultIdentity<IdentityUser> to AddDefaultIdentity<Employee>. Now add-migration and update-database
+    5. Now we need to add two tables, the leavetype table and LeaveAllocation table. Since these tables have common coloumns, we can create another class with the common coloumns
+       and then inherit this class onto the LeaveType and LeaveAllocation class. Now add migra and update database
+    
+    
+    asjkd*/
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +21,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
